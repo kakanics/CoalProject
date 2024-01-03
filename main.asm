d883c27a1f55110787ef4720361ca717c9439b0a
kakanics <affan.bb7@gmail.com>
2024-01-04 03:43:35 +0500

    source code

---
+++ main.asm
@@ -0,0 +1,126 @@
+; This example program displays different types of data on a 16x2 LCD.
+;
+; Data Pin Connections for 16x2 LCD
+; [LCD pins]          [Arduino UNO Pins]
+; RS   ---------------  8 (PB0)
+; E    ---------------  9 (PB1)
+; D4   ---------------  4 (PD4)
+; D5   ---------------  5 (PD5)
+; D6   ---------------  6 (PD6)
+; D7   ---------------  7 (PD7)
+; A    ---------------  13 (PB5)	; Anode pin of LCD Backlight LED
+
+.include "m328pdef.inc"
+.include "delay_Macro.inc"
+.include "1602_LCD_Macros.inc"
+.include "UART_Macros.inc"
+
+.cseg
+.org 0x0000
+
+SBI DDRB, PB3            ; Set PB3 pin for Output to LED
+CBI PORTB, PB3           ; LED OFF
+
+SBI DDRB, PB4            ; Set PB4 pin for Output to LED
+CBI PORTB, PB4           ; LED OFF
+
+.def A  = r16            ; Rename or attach a label to the register
+.def AH = r17	
+
+; ADC config
+LDI   A, 0b11000111      ; [ADEN ADSC ADATE ADIF ADIE ADIE ADPS2 ADPS1 ADPS0]
+STS   ADCSRA, A          
+LDI   A, 0b01100000      ; [REFS1 REFS0 ADLAR – MUX3 MUX2 MUX1 MUX0]
+STS   ADMUX, A           ; Select ADC0 (PC0) pin
+SBI   PORTC, PC0         ; Enable Pull-up Resistor
+
+Serial_begin
+LCD_init                 ; Initialize the 16x2 LCD
+LCD_backlight_OFF        ; Turn Off the LCD Backlight
+delay 1000
+LCD_backlight_ON         ; Turn On the LCD Backlight
+delay 300
+
+; Initialize variables for strings and lengths
+LDI ZL, LOW (2 * dark_string)
+LDI ZH, HIGH (2 * dark_string)
+LDI R20, DarkString_len
+LCD_send_a_string
+delay 500
+
+loop:
+    LDS  A, ADCSRA        ; Start Analog to Digital Conversion
+    ORI  A, (1<<ADSC)
+    STS  ADCSRA, A
+
+    wait:
+        LDS  A, ADCSRA    ; Wait for ADC conversion to complete
+        sbrc A, ADSC
+        rjmp wait
+
+    LDS  A, ADCL          ; Must Read ADCL before ADCH
+    LDS  AH, ADCH
+    delay 100              ; Delay 100ms
+
+    cpi  AH, 200           ; Compare LDR reading with the desired threshold value (e.g., 200)
+    brlo LED_OFF           ; Jump if AH < 200
+    SBI  PORTB, PB4        ; LED ON
+    CBI  PORTB, PB3        ; LED OFF
+    rjmp LED_ON_End        ; Jump to skip LED_OFF condition
+
+LED_OFF:
+    CBI  PORTB, PB4        ; LED OFF
+    SBI  PORTB, PB3        ; LED ON
+
+LED_ON_End:
+    ; LCD Configuration
+    LCD_send_a_command 0x80
+    delay 20
+
+    ; Scroll text on LCD
+    LCD_send_a_command 0x14
+    LCD_send_a_command 0x14
+    LCD_send_a_command 0x14
+    LCD_send_a_command 0x14
+    LCD_send_a_command 0x14
+    LCD_send_a_command 0x14
+    LCD_send_a_command 0x14
+    LCD_send_a_command 0x14
+    LCD_send_a_command 0x14
+
+    ; Send data to UART and LCD
+    Serial_writeReg AH   ; Send register data to UART
+    delay 50
+
+    LCD_send_a_register AH ; Send register data to LCD
+    delay 50
+
+    ; Display spaces on LCD
+    LDI ZL, LOW (2 * spaces)
+    LDI ZH, HIGH (2 * spaces)
+    LDI R20, SpacesString_len
+    LCD_send_a_string
+    delay 50
+
+rjmp loop
+
+dark_string: .db "Darkness:", 0
+spaces:      .db "   ", 0
+
+; Calculate string lengths
+len: .equ DarkString_len   = (2 * (len - dark_string)) - 1
+Spacelen: .equ SpacesString_len = (2 * (len - spaces)) - 1
+
+; It is recommended to define constants (arrays, strings, etc.) at the end of the code segment
+; .db directive is used to declare constants
+; The length of the string must be an even number of bytes
+
+; ***************************************************************************
+; * Code written by:                                                      *
+; * Syed Tehseen ul Hasan Shah                                             *
+; * Lecturer, University of Engineering and Technology Lahore, Pakistan    *
+; * 24-December-2023                                                      *
+; ***************************************************************************
+
+
+(ye code improve kr k Diya isne comment wagera daal k ye dekhlo(wese mujja to wesa ka wesa hi lgra)
---
+++ source.cpp
@@ -0,0 +1,211 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#include "ThingSpeak.h"
+
+// Wi-Fi credentials
+const char *ssid = "Galaxy";
+const char *password = "fzvg0533";
+
+// MQTT configuration
+const char *mqttServer = "broker.hivemq.com";
+const int mqttPort = 1883;
+const char *mqttClientId = "2022-CS-178";
+const char *outputTopic = "esp32/output";
+WiFiClient espClient;
+
+// ThingSpeak configuration
+unsigned long myChannelNumber = 2391804;
+const char *myWriteAPIKey = "T7K0BZL98517507L";
+String myStatus = "";
+
+// Firebase configuration
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+bool signupOK = false;
+
+static unsigned long lastMillis = 0;
+PubSubClient client(espClient);
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial2.begin(9600);
+    WifiSetup();
+
+    // configure cloud services
+    fireBaseSetup();
+    ThingSpeak.begin(espClient);
+    ThingSpeak.setStatus(myStatus);
+
+    // Configure MQTT
+    client.setServer(mqttServer, mqttPort);
+    client.setCallback(callBack);
+    connectToMQTT();
+}
+
+void WifiSetup()
+{
+    // Connect to Wi-Fi
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+
+    // keep retrying until successful connection
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void fireBaseSetup()
+{
+    // configure firebase
+    config.api_key = "AIzaSyBUr_U4sLQYtNtjkn_j0LPPXv0IVaCSu08";
+    config.database_url = "https://ldr-mqtt-esp-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+    // try to connect
+    if (Firebase.signUp(&config, &auth, "", ""))
+    {
+        Serial.println("ok");
+        signupOK = true;
+    }
+    else
+    {
+        Serial.printf("%s\n", config.signer.signupError.message.c_str());
+    }
+    config.token_status_callback = tokenStatusCallback;
+
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
+}
+
+void callBack(char *inputTopic, byte *message, unsigned int length)
+{
+    // Callback function for MQTT messages
+    Serial.print("Message arrived on topic: ");
+    Serial.print(inputTopic);
+    Serial.print(". Message: ");
+    String messageTemp;
+
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print((char)message[i]);
+        messageTemp += (char)message[i];
+    }
+    Serial.println();
+}
+
+void loop()
+{
+    int data = -1; //-1 for data=not read
+
+    // reconnect to wifi if disconnected
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        WifiSetup();
+    }
+    // Handle MQTT events
+    if (client.connected() == false)
+    {
+        connectToMQTT();
+    }
+    client.loop();
+
+    // read data from arduino
+    if (Serial2.available() > 0)
+    {
+        data = Serial2.read();
+        Serial.println(data);
+        // clear the buffer in case there are too many messages
+        while (Serial2.available())
+        {
+            Serial2.read();
+        }
+    }
+
+    // Publish a message every 5 seconds
+    if (millis() - lastMillis > 1000 && data != -1)
+    {
+        char message[50];
+        snprintf(message, sizeof(message), "Light Intensity: %d", data);
+        publishMessage(message);
+        Serial.println(message);
+
+        // publish data to cloud
+        ThingSpeakPublishData(1, data);
+        firebasePublishData("test/int", data);
+    }
+    delay(100);
+}
+
+void ThingSpeakPublishData(int field, int data)
+{
+    // Publish data to ThingSpeak
+    ThingSpeak.setField(1, data);
+    int httpCode = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+    if (httpCode == 200)
+    {
+        Serial.println("Channel update successful.");
+    }
+    else
+    {
+        Serial.println("Problem updating channel. HTTP error code " + String(httpCode));
+    }
+}
+
+void firebasePublishData(String address, int data)
+{
+    // Publish data to Firebase
+    if (Firebase.ready() && signupOK)
+    {
+        if (Firebase.RTDB.setInt(&fbdo, address, data))
+        {
+            Serial.println("PASSED");
+            Serial.println("PATH: " + fbdo.dataPath());
+            Serial.println("TYPE: " + fbdo.dataType());
+        }
+        else
+        {
+            Serial.println("FAILED");
+            Serial.println("REASON: " + fbdo.errorReason());
+        }
+    }
+}
+
+void connectToMQTT()
+{
+    while (!client.connected())
+    {
+        Serial.println("Connecting to MQTT...");
+        if (client.connect(mqttClientId))
+        {
+            Serial.println("Connected to MQTT");
+        }
+        else
+        {
+            Serial.print("Failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
+}
+
+void publishMessage(const char *message)
+{
+    if (client.connected())
+    {
+        client.publish(outputTopic, message);
+        Serial.println("Message Published");
+    }
+}
